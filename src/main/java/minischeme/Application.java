09454010c86855a12187d3ce170bdb5311b1e313
sakuragts <sakura_gts@hotmail.com>
2020-05-08 23:56:26 -0400

    changer le parser

--- src/main/java/minischeme/Application.java
+++ src/main/java/minischeme/Application.java
@@ -1,6 +1,6 @@
 package minischeme;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import minischeme.parser.api.Parser;
 
 import java.io.File;
 import java.util.List;
@@ -8,8 +8,8 @@ import java.util.List;
 public class Application {
 
   public static void main(String... args) throws Exception {
-    List<Object> code = new ObjectMapper().readValue(new File(args[0]), List.class);
+    List<Object> code = new Parser().readValue(new File(args[0]), List.class);
     Object result = new Evaluator().eval(code, GlobalEnvironment.build());
-    System.out.println(new ObjectMapper().writeValueAsString(result));
+    System.out.println(new Parser().writeValueAsString(result));
   }
 }
