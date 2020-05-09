fd96cb6d14c19fa71940d1ad65d5761e0c5837e7
sakuragts <sakura_gts@hotmail.com>
2020-05-09 14:08:24 -0400

    arranger la branche

--- src/main/java/Application2.java
+++ src/main/java/Application2.java
@@ -1,6 +1,6 @@
 package minischeme;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import minischeme.parser.api.Parser;
 
 import java.io.File;
 import java.util.List;
@@ -8,8 +8,9 @@ import java.util.List;
 public class Application {
 
   public static void main(String... args) throws Exception {
-    List<Object> code = new ObjectMapper().readValue(new File(args[0]), List.class);
+    String source = "";
+    List<Object> code = new Parser().parseString(source);
     Object result = new Evaluator().eval(code, GlobalEnvironment.build());
-    System.out.println(new ObjectMapper().writeValueAsString(result));
+    System.out.println(new Parser().parseString(result));
   }
 }
