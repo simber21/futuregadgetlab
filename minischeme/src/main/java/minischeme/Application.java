9cfad023023559c7ef4fa0074b7ff047f31d0dd2
Simberg <simonbergeron21@gmail.com>
2020-05-10 19:06:09 -0400

    Changement pour le nouveau Parser.

--- minischeme/src/main/java/minischeme/Application.java
+++ minischeme/src/main/java/minischeme/Application.java
@@ -1,17 +1,19 @@
 package minischeme;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import minischeme.parser.api.Parser;
 
-import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.List;
 
 public class Application {
 
   public static void main(String... args) throws Exception {
-   
-    List<Object> code = new ObjectMapper().readValue(new File(args[0]), List.class);
+    Path path = Paths.get(args[0]);
+    String source = Files.readString(path);
+    List<Object> code = new Parser().parseString(source);
     Object result = new Evaluator().eval(code, GlobalEnvironment.build());
-    System.out.println(new ObjectMapper().writeValueAsString(result));
-    
+    System.out.println(result);
   } 
 }
